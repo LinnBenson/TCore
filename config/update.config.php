fd3825e7d3b49f4f0ce9f6f523ba61496dda71f6
LinnBenson <bc@tobc.me>
2025-03-11 04:46:19 +0800

    首次更新

--- config/update.config.php
+++ config/update.config.php
@@ -2,7 +2,12 @@
     return [
         'version' => '1.0.0',
         'file' => [
-
+            'config/update.config.php',
+            'public/index.php',
+            '.gitignore',
+            'LICENSE',
+            'README.md',
+            'update'
         ],
         'dir' => [
 
